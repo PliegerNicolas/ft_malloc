216df469ed3bf6cc346580e0ec92c9176a267883
PliegerNicolas <plieger05@gmail.com>
2025-04-27 22:29:07 +0200

    Corrected algorithms to find best free .

--- sources/internal/mchunk/find_mbin_best_fit_free_mchunk.c
+++ sources/internal/mchunk/find_mbin_best_fit_free_mchunk.c
@@ -18,22 +18,35 @@
 
 /**
  * @brief Find the first `mchunk_t` able to contain `allocation_size` in given mbin.
+ *
  * @param mbin Initial `mchunk_t` of free `mchunk_t` list (`mregion_t`->mbin).
  * @param allocation_size Expected amount of bytes to store in requested `mchunk_t`.
  * 
  * @returns First fit free `mchunk_t` or `NULL`.
+ * Can find an exact match or a `mchunk_t` that can be partitionned into a new one.
 */
 static mchunk_t    *find_mbin_mchunk_first_fit(mchunk_t **mbin, size_t allocation_size)
 {
     mchunk_t    *current_mchunk;
+    size_t      partitioned_mchunk_size;
+
     if (!mbin)
         return NULL;
 
-    current_mchunk = *mbin;
-    while(current_mchunk && current_mchunk->allocation_size < allocation_size)
-        current_mchunk = current_mchunk->next_free_mchunk;
+    partitioned_mchunk_size = map_allocation_size_to_mchunk_size(allocation_size);
 
-    return current_mchunk;
+    current_mchunk = *mbin;
+    while (current_mchunk)
+    {
+        if (current_mchunk->allocation_size == allocation_size)
+            return current_mchunk;
+        if (current_mchunk->allocation_size >= partitioned_mchunk_size)
+            return current_mchunk;
+
+        current_mchunk = current_mchunk->next_free_mchunk;
+    }
+
+    return NULL;
 }
 
 /**
@@ -42,25 +55,33 @@ static mchunk_t    *find_mbin_mchunk_first_fit(mchunk_t **mbin, size_t allocatio
  * @param allocation_size Expected amount of bytes to store in requested `mchunk_t`.
  * 
  * @returns Smallest fit free `mchunk_t` or `NULL`.
+ * Can find an exact match or a `mchunk_t` that can be partitionned into a new one.
 */
 static mchunk_t    *find_mbin_mchunk_best_fit(mchunk_t **mbin, size_t allocation_size)
 {
+    mchunk_t    *current_mchunk;
     mchunk_t    *best_fit_mchunk;
+    size_t      partitioned_mchunk_size;
 
     if (!mbin)
         return NULL;
 
+    partitioned_mchunk_size = map_allocation_size_to_mchunk_size(allocation_size);
     best_fit_mchunk = NULL;
-    for (mchunk_t *current_mchunk = *mbin; current_mchunk; current_mchunk = current_mchunk->next_free_mchunk)
-    {
-        if (current_mchunk->allocation_size < allocation_size)
-            continue;
 
+    current_mchunk = *mbin;
+    while (current_mchunk)
+    {
         if (current_mchunk->allocation_size == allocation_size)
             return current_mchunk;
 
-        if (!best_fit_mchunk || (current_mchunk->allocation_size < best_fit_mchunk->allocation_size))
-            best_fit_mchunk = current_mchunk;
+        if (current_mchunk->allocation_size >= partitioned_mchunk_size)
+        {
+            if (!best_fit_mchunk || best_fit_mchunk->allocation_size > current_mchunk->allocation_size)
+                best_fit_mchunk = current_mchunk;
+        }
+
+        current_mchunk = current_mchunk->next_free_mchunk;
     }
 
     return best_fit_mchunk;
@@ -72,22 +93,32 @@ static mchunk_t    *find_mbin_mchunk_best_fit(mchunk_t **mbin, size_t allocation
  * @param allocation_size Expected amount of bytes to store in requested `mchunk_t`.
  * 
  * @returns First good enough size fit free `mchunk_t` or `NULL`.
+ * Can find an exact match or a `mchunk_t` that can be partitionned into a new one.
 */
 static mchunk_t    *find_mbin_mchunk_good_enough_fit(mchunk_t **mbin, size_t allocation_size)
 {
     mchunk_t    *current_mchunk;
+    size_t      partitioned_mchunk_size;
     size_t      good_enough_size_threshold;
 
     if (!mbin)
         return NULL;
 
-    good_enough_size_threshold = allocation_size * 1.15;
+    partitioned_mchunk_size = map_allocation_size_to_mchunk_size(allocation_size);
+    good_enough_size_threshold = partitioned_mchunk_size * 1.15;
 
     current_mchunk = *mbin;
-    while (current_mchunk && current_mchunk->allocation_size < good_enough_size_threshold)
-        current_mchunk = current_mchunk->next_free_mchunk;
+    while (current_mchunk)
+    {
+        if (current_mchunk->allocation_size == allocation_size)
+            return current_mchunk;
+        if (current_mchunk->allocation_size >= partitioned_mchunk_size && current_mchunk->allocation_size <= good_enough_size_threshold)
+            return current_mchunk;
 
-    return current_mchunk;
+        current_mchunk = current_mchunk->next_free_mchunk;
+    }
+
+    return NULL;
 }
 
 /* *************************************************************************** */
